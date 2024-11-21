02f4dbc054069c73c1fab9dac61602b8e18ff0bc
Jordi Ribellas <jordiribellasramos@gmail.com>
2024-11-21 13:00:53 +0100

    added the dependence

--- src/main/java/com/jordiribellas/microservices/api_gateway/LoggingFilter.java
+++ src/main/java/com/jordiribellas/microservices/api_gateway/LoggingFilter.java
@@ -4,6 +4,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.cloud.gateway.filter.GatewayFilterChain;
 import org.springframework.cloud.gateway.filter.GlobalFilter;
+import org.springframework.stereotype.Component;
 import org.springframework.web.server.ServerWebExchange;
 
 import reactor.core.publisher.Mono;
